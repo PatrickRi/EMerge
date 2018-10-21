2fe9c3441516b7af0c55d8e0c7e19c886860722e
PatrickRi <pat.riemer@gmail.com>
2018-10-21 17:55:55 +0200

    change git repo to github one

--- Complete/config-server/src/main/java/com/emerge/complete/configserver/ConfigServerApplication.java
+++ Complete/config-server/src/main/java/com/emerge/complete/configserver/ConfigServerApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableEurekaClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
